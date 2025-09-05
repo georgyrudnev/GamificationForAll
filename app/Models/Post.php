a458b7d8c0b8ad0d70284b349cc17b30be919df4
georgyrudnev <georgy.rudnev@outlook.de>
2025-09-05 12:59:27 +0200

    Finished postsModelv1, factories & posts table

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\PostFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /** @use HasFactory<PostFactory> */
+    use HasFactory;
+
+    public function sender() {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+    public function receiver() {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+}
