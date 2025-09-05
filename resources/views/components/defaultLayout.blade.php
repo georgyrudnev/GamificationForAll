b6182973d790ed3e76a7a67860342353b5451483
georgyrudnev <georgy.rudnev@outlook.de>
2025-09-05 13:00:38 +0200

    created default layout

---
+++ resources/views/components/defaultLayout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<header class="bg-pink-500 flex-row align-content items-center justify-center ">
+    <div class="align-items"> What is the difference between header and head in html? Where do I put my cdn scripts?
+    </div>
+    <h1 class="bg-blue-500"> Bravour
+    </h1>
+    <div> Bravour </div>
+</header>
+
+<body>
+
+{{$slot}}
+
+</body>
+
+
+<footer class="bg-yellow-500 grid justify-items-center">
+    This is the footer
+    <div class="bg-blue-200">
+        this is div inside footer
+         </div>
+</footer>
