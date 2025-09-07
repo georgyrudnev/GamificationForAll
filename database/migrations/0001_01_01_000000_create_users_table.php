ce2c701bc9c446a26c17ebfa85263f5b2da10825
georgyrudnev <georgy.rudnev@outlook.de>
2025-09-07 22:43:10 +0200

    FEATURE Added points as user attrib in db

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->integer('score')->default(1000);
+            $table->integer('points')->default(2000);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
