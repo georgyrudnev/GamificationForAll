16a8be65b5689ec3fbd1c6d92a55655fe9e410fd
georgyrudnev <georgy.rudnev@outlook.de>
2025-10-03 21:29:51 +0200

    FEATURE homepage $ layout revamp

--- resources/views/components/defaultLayout.blade.php
+++ resources/views/components/defaultLayout.blade.php
@@ -13,16 +13,45 @@
 
     <h1 class="w-96 text-5xl font-bold"> Bravour
     </h1>
-    <div class="w-64">
+    <div id="score" class="w-64">
         <img src="{{ asset("img/score.png")}}" alt="score symbol" class="h-20 w-16 ml-24">
         Score: @yield('Score')
     </div>
-    <div>
+    <div id="points">
         <img src="{{ asset("img/spendingPoints.png")}}" alt="score symbol" class="h-20 w-16 ml-6">
         <div class="ml-6">
             Points: @yield('Points')
         </div>
     </div>
+
+  <!--  <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden"> -->
+        @if (Route::has('login'))
+            <nav class="ml-auto gap-4 p-8">
+                @auth
+                    <a
+                        href="{{ url('/dashboard') }}"
+                        class="mr-80 px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                    >
+                        Reporting
+                    </a>
+                @else
+                    <a
+                        href="{{ route('login') }}"
+                        class="px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                    >
+                        Log in
+                    </a>
+
+                    @if (Route::has('register'))
+                        <a
+                            href="{{ route('register') }}"
+                            class="px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                            Register
+                        </a>
+                    @endif
+                @endauth
+            </nav>
+        @endif
 </header>
 
 <body class="bg-orange-600">
@@ -32,9 +61,3 @@
 </body>
 
 
-<footer class="bg-yellow-500 grid justify-items-center">
-    This is the footer
-    <div class="bg-blue-200">
-        this is div inside footer
-         </div>
-</footer>
